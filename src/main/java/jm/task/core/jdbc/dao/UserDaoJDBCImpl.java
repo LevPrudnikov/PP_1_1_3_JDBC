1a664e072d2455235485ce942ba4995cdcbdaa36
Lev <levka54@gmail.com>
2025-01-14 20:51:08 +0700

    correction code try 4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -19,51 +19,78 @@ public class UserDaoJDBCImpl implements UserDao {
         String sql = "CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT,name VARCHAR(45) NOT NULL, " +
                 "lastname VARCHAR(45) NOT NULL, age TINYINT(3) NULL, " +
                 "PRIMARY KEY (`id`))";
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
+        Connection connection = Util.getConnection();
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
             connection.commit();
         } catch (SQLException ignore) {
+            try {
+                connection.rollback();
+            } catch (SQLException ignored) {}
         }
+        try {
+            connection.close();
+        } catch (SQLException ignore) {}
     }
 
     public void dropUsersTable() {
         String sql = "DROP TABLE IF EXISTS users";
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
+        Connection connection = Util.getConnection();
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
             connection.commit();
+        } catch (SQLException ignore) {
+            try {
+                connection.rollback();
+            } catch (SQLException ignored) {}
+        }
+        try {
+            connection.close();
         } catch (SQLException ignore) {}
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
         String sql = "INSERT INTO users (name, lastname, age) VALUES (?, ?, ?)";
-        try (Connection connection = Util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        Connection connection = Util.getConnection();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setInt(3, age);
             preparedStatement.executeUpdate();
             connection.commit();
+        } catch (SQLException ignore) {
+            try {
+                connection.rollback();
+            } catch (SQLException ignored) {}
+        }
+        try {
+            connection.close();
         } catch (SQLException ignore) {}
     }
 
     public void removeUserById(long id) {
         String sql = "DELETE FROM users WHERE id = ?";
-        try (Connection connection = Util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        Connection connection = Util.getConnection();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
             connection.commit();
+        } catch (SQLException ignore) {
+            try {
+                connection.rollback();
+            } catch (SQLException ignored) {}
+        }
+        try {
+            connection.close();
         } catch (SQLException ignore) {}
     }
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
         String sql = "SELECT * FROM users";
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement();
+        Connection connection = Util.getConnection();
+        try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
             while (resultSet.next()) {
                 User user = new User();
@@ -75,16 +102,30 @@ public class UserDaoJDBCImpl implements UserDao {
                 users.add(user);
             }
             connection.commit();
+        } catch (SQLException ignore) {
+            try {
+                connection.rollback();
+            } catch (SQLException ignored) {}
+        }
+        try {
+            connection.close();
         } catch (SQLException ignore) {}
         return users;
     }
 
     public void cleanUsersTable() {
         String sql = "TRUNCATE TABLE users";
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
+        Connection connection = Util.getConnection();
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
             connection.commit();
+        } catch (SQLException ignore) {
+            try {
+                connection.rollback();
+            } catch (SQLException ignored) {}
+        }
+        try {
+            connection.close();
         } catch (SQLException ignore) {}
     }
 }
